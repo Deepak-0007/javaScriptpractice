3771487ad0fda885f33d01b202af67a959fa7629
Deepak-0007 <dy124507@gmail.com>
2023-07-11 23:16:04 +0530

    opperator

--- opperator.js
+++ opperator.js
@@ -4,7 +4,7 @@
 var a=10;  
 var b=20;  
 var c=a+b;//It adds values of a and b variable  
-document.write(c);//prints sum of 10 and 20  
+document.write(c);//prints sum of 10 and 20 are in c
 </script>   
 </body>
 </html>
