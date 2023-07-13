3f68090a95ae0032611699b4ce567edb5f8b35e2
Deepak-0007 <dy124507@gmail.com>
2023-07-13 23:32:37 +0530

    let_const_var

---
+++ let_var_const.js
@@ -0,0 +1,15 @@
+var a =50;
+let b="deepak";
+var c= null
+const author="Deepak" // const cannot change
+var d= undefined
+{
+    let b= "this";
+    console.log(b) // let is block spaced
+}
+console.log(b)
+{
+    var a=20;
+    console.log(a)// all the where 
+}
+console.log(a)
