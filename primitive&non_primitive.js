75a8c23bfb00d6cae3bc57955e73c85dfd7a4409
Deepak-0007 <dy124507@gmail.com>
2023-07-14 22:40:53 +0530

    primitive and non primative datatype

---
+++ primitive&non_primitive.js
@@ -0,0 +1,18 @@
+let a=null;
+let b="Deepak"
+let c= 345;
+let d= true; 
+let e= BigInt("567")
+let f= undefined
+console.log(a,b,c,d,e,f)
+console.log(typeof d)
+
+//object in js
+const item={
+    "Deepak": true,
+    "Ajay": false,
+    "lokesh": 67,
+    "Ravi":undefined
+
+}
+console.log(item["lokesh"])
