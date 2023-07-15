d61db0eaad7dd2a42520a0aea73128e4abfad959
Deepak-0007 <dy124507@gmail.com>
2023-07-15 23:09:05 +0530

    practice

---
+++ practice_set_1.js
@@ -0,0 +1,18 @@
+// create a variable string and add other variable with number
+let a= "Deepak"
+let b=7;
+console.log(a+b)
+  
+// type of varialbe and use to find data type
+console.log(typeof(a+b))
+
+//create a  const object and can you change it into number later
+const a1 = {
+    name:"Deepak",
+    section:6,
+    isPrinciple : false
+}
+//a1=45; you can not change const variable object in string and number 
+a1['friend']="Ajay"
+a1['name']="Harinder"
+console.log(a1)
