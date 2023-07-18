19b7fb0d27ba882821b7d3420864acd9cb8964ff
Deepak-0007 <dy124507@gmail.com>
2023-07-18 23:11:18 +0530

    update

--- milestone/exA1#3task4.js
+++ milestone/exA1#3task4.js
@@ -37,7 +37,78 @@
   
 //  }
 
-n = 4;
-for (i = 1; i <= n; i++) {
-    console.log(i*i);
+// n = 4;
+// for (i = 1; i <= n; i++) {
+//     console.log(i*i);
+// }
+
+// let pattern = "";
+// let n=3;
+// for (let i = 1; i <=n; i++) {
+//   pattern += "A"+i;
+//   console.log(pattern);
+// }
+// let n=4;
+// for (let i = 1; i <=n; i++) {
+//   let a = i + 1;
+//   let b = 2 * i + 1;
+//   console.log(i + "+" + a + "=" + b);
+// }
+
+  // let n=4;
+  // let output = "";
+  // for (let i = 1; i <= 2*n; i += 2) {
+  //   output += "XY" + i + (i+1);
+  // }
+  // console.log(output);
+
+  // let n=5;
+  //   let output = " ";
+  //   let b="";
+  //   for (let i = 1; i <= n; i++) {
+  //   b+="@";
+  //   output +=i+b;
+  //   }
+  // console.log(output);
+
+  // let n=4;
+  //   let output = " ";
+  //   let b="";
+  //   for (let i = 1; i <= n; i++) {
+  //   b+="M";
+  //   output +=b+"+";
+    
+  //   }
+  // console.log(output);
+
+
+  // let n=4;
+  // for(i=1; i<=n;i++){
+  //   let a=i*3;
+  //   console.log(a);
+  // }
+
+  // let n=3;
+  // for(i=1; i<=n;i++){
+  //   let a=i*3-2;
+  //   console.log(a);
+  // }
+
+// let n=4;
+// let b="";
+//   for(i=n; i>=1;i--){
+//     console.log(i);
+//   }
+  
+let n = 3;
+let b = "";
+for (i = n; i >= 1; i--) {
+  b += i;
 }
+console.log(b)
+
+
+
+  
+
+  
