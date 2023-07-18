75c9f79431a843f4d227cc0b0507845b676e0930
Deepak-0007 <dy124507@gmail.com>
2023-07-18 23:12:29 +0530

    updated

---
+++ milestone/exA1#4tasks.js
@@ -0,0 +1,146 @@
+// let n=1;
+// let remainder=n%2;
+// if(remainder==1){
+//     n++;
+// }
+// console.log(n);
+
+// let num=60;
+// if(num%6==0){
+//     console.log('divisible by 6 ');
+// }
+// else if(num%3==0){
+//     console.log('divisible by 2');
+// }
+// else{
+//     console.log('Not divisible 2,3,and 6');
+// }
+
+// let size =6;
+// let str= '';
+// for(let i=1; i<=size; i++){
+//     if(i%2==1){
+//         str=str +'j'
+//     }
+//     else{
+//     str = str+'k';
+//     }
+// }
+// console.log(str);
+
+//  let n=21;
+//  let count=0;
+//  for(let i=2; i<n; i++){
+//     if(n%i==0){
+//         count++;
+//     }
+// }
+//     if (count==0){
+//         if(n==1){
+//             console.log('1 is neighter prime or composite number');
+//         }
+//         else{
+//         console.log('prime');
+//         }
+//     }
+//     else{
+//         console.log('composite');
+//     }
+
+
+//  let n=-7;
+//  if(n>0){
+//     console.log("positive number");
+//  }
+//  else{
+//     console.log("not a positive number");
+//  }
+
+// let n=28;
+// if(n%10==0){
+//     console.log("multiple fo 10");
+// }
+// else{
+//     console.log("not multiple of 10");
+// }
+
+// let m=6;
+// let n=8;
+// if(m>n){
+//     console.log("larger")
+// }
+// else if(m==n){
+//     console.log("Equal");
+// }
+// else {
+//     console.log("smaller");
+// }
+
+// let b=15;
+//   for(let i=2;i<=5;i++){
+//     if(b%i==0){
+//         console.log(i+": yes")
+        
+//     }
+//     else{
+       
+//         console.log(i+": no")
+//     }
+//   }
+// let n=4;
+//   for(let i=1;i<=n;i++){
+//     if(i%2!==0){
+//         console.log(i+": odd")
+        
+//     }
+//     else{
+       
+//         console.log(i+": even")
+//     }
+//   }
+
+// let n=15;
+// for(let i=1; i<=n; i++){
+//     if(i%5==0){
+//         console.log("hello");
+//     }
+// }
+
+// let n=17; 
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i == 0) {
+//             console.log(i);
+//         }
+//     }
+
+// let n=5 
+//     for (let  i = 1; i <= n; i++) {
+//         let square = i * i;
+//          if(square % 2 == 0){
+//             console.log("even")
+//          }
+//         console.log(`Square of ${i} = ${square} : ${type}`);
+//     }
+
+
+
+// let c=5;
+
+// for(let i=1; i<=c;i++){
+//     let square=i*i;
+//     if(square%2==0){
+//         console.log("square of"+i+"="+square+ ":even")
+//     }
+//     else {
+//         console.log("square of"+i+"="+square+":odd");
+//     }
+// }
+
+let o=85;
+  if (o >= 75) {
+    console.log('Good');
+  } else if (o >= 50) {
+    console.log('Average');
+  } else {
+    console.log('Poor');
+  }
